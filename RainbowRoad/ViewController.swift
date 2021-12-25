011d80ed136d228ac11abf7675dd77cd9a901465
plateauh <70834431+plateauh@users.noreply.github.com>
2021-12-25 23:46:33 +0300

    colors.count change

--- RainbowRoad/ViewController.swift
+++ RainbowRoad/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 6
+        return colors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
